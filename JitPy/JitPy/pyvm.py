e403f8d382d36b5a701cfc6d21a93b2dd81969d4
ftxj <932141413@qq.com>
2023-01-11 22:53:13 +0800

    [JitPy] Support Nested Call

--- JitPy/JitPy/pyvm.py
+++ JitPy/JitPy/pyvm.py
@@ -13,12 +13,18 @@ class Functions(object):
         self.name = name
         self.bin = bin
 
+class ByteCodeFn(object):
+    def __init__(self, name, code):
+        self.name = name
+        self.bin = dis.Bytecode(code)
+        
 class Frame(object):
-    def __init__(self):
+    def __init__(self, locals={}, globals={}, fast=[]):
         self.block_stack = []
         self.data_stack = []
-        self.f_locals = {}
-        self.f_globals = {}
+        self.fast = fast
+        self.f_locals = locals
+        self.f_globals = globals
         self.f_builtins = {}
         self.f_builtins["print"] = BuildinFunctions("print", print)
 
@@ -67,16 +73,16 @@ binaryOperationList = {
 
 class VirtualMachine(object):
     def __init__(self):
-        self.frame = []
-        self.frame.append(Frame())
+        self.frames = []
+        self.frames.append(Frame())
         self.instructions = []
-        self.pc = 0
+        self.pc = []
 
     def push(self, elm):
-        self.frame[0].push_data(elm)
+        self.frames[-1].push_data(elm)
 
     def pop(self):
-        return self.frame[0].pop_data()
+        return self.frames[-1].pop_data()
 
     def binaryOp(self, op):
         lhs = self.pop()
@@ -88,7 +94,7 @@ class VirtualMachine(object):
         self.push(name)
 
     def LOAD_NAME(self, inst):
-        frame = self.frame[0]
+        frame = self.frames[-1]
         name = inst.argval
         if name in frame.f_locals:
             val = frame.f_locals[name]
@@ -100,9 +106,14 @@ class VirtualMachine(object):
             raise NameError("name '%s' is not defined" % name)
         self.push(val)
 
+    def LOAD_FAST(self, inst):
+        val = self.frames[-1].fast[-1]
+        self.frames[-1].fast.pop()
+        self.push(val)
+
     def STORE_NAME(self, inst):
         name = inst.argval
-        self.frame[-1].f_locals[name] = self.pop()
+        self.frames[-1].f_locals[name] = self.pop()
 
     def BINARY_OP(self, inst):
         self.binaryOp(inst.argrepr)
@@ -123,17 +134,22 @@ class VirtualMachine(object):
             params.append(self.pop())
         params.reverse()
         fn = self.pop()
-        if fn.name in self.frame[-1].f_builtins:
-            fn.bin(tuple(params))
+        fn.bin(tuple(params))
     
     def CALL_FUNCTION(self, inst):
         num_params = inst.arg
         params = []
         for i in range(num_params):
             params.append(self.pop())
-        params.reverse()
         fn = self.pop()
-        self.push(fn.bin(tuple(params)))
+
+        if(isinstance(fn, ByteCodeFn)):
+            frame = Frame(globals=self.frames[-1].f_globals, fast=params)
+            self.frames.append(frame)
+            self.push(self.run(fn.bin))
+        else:
+            params.reverse()
+            self.push(fn.bin(tuple(params)))
 
     def RESUME(self, inst):
         pass
@@ -141,22 +157,23 @@ class VirtualMachine(object):
         pass
 
     def RETURN_VALUE(self, inst):
-        self.pop()
+        return self.pop()
 
     def MAKE_FUNCTION(self, inst):
-        fn = self.pop()
+        name = self.pop()
+        code = self.pop()
+        self.push(ByteCodeFn(name, code))
 
     def IMPORT_NAME(self, inst):
         fromlist = self.pop()
         level = self.pop()
-        frame = self.frame[-1]
+        frame = self.frames[-1]
         self.push(
             __import__(inst.argval, frame.f_globals, frame.f_locals, fromlist, level)
         )
 
-
     def LOAD_METHOD(self, inst):
-        frame = self.frame[-1]
+        frame = self.frames[-1]
         name = inst.argval
         namespace_num = inst.arg
         namespace = []
@@ -177,23 +194,35 @@ class VirtualMachine(object):
         self.push(fn.bin(tuple(params)))
 
     def step(self):
-        inst = self.instructions[self.pc]
-        getattr(self, inst.opname)(inst)
+        inst = self.instructions[-1][self.pc[-1]]
+        # print(inst.opname)
+        return getattr(self, inst.opname)(inst)
 
     def run(self, code):
-        print(dis.dis(code))
-        self.instructions = list(map(convert_instruction, dis.get_instructions(code)))
-        self.pc = 0
-        while(self.pc < len(self.instructions)):
-            self.step()
-            self.pc = self.pc + 1
+        self.pc.append(0)
+        if(isinstance(code, dis.Bytecode)):
+            self.instructions.append(list(map(convert_instruction, code)))
+        else:
+            print(dis.dis(code))
+            self.instructions.append(list(map(convert_instruction, dis.get_instructions(code))))
+        ret = None
+        while(self.pc[-1] < len(self.instructions[-1])):
+            ret = self.step()
+            self.pc[-1] = self.pc[-1] + 1
+        self.pc.pop()
+        self.instructions.pop()
+        self.frames.pop()
+        return ret
 
 def test():
     code = '''
 import torch
 a = torch.randn(3,3)
 b = torch.randn(3,3)
+def foo(a):
+    return a
 print(a + b)
+print(foo(23) + foo(33))
 '''
     vm = VirtualMachine()
     vm.run(code)
