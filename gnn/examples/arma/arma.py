dc797fff10facf72fb3b3c5207f28e87780103ef
ftxj <932141413@qq.com>
2023-02-21 04:47:21 +0000

    Perf Stage test

--- gnn/examples/arma/arma.py
+++ gnn/examples/arma/arma.py
@@ -17,6 +17,9 @@ import time
 import ctypes
 _cudart = ctypes.CDLL('libcudart.so')
 ret = _cudart.cudaProfilerStart()
+torch._C._jit_set_nvfuser_enabled(True)
+
+
 
 def act_fn(x):
     return x
@@ -30,58 +33,151 @@ class Net(torch.nn.Module):
         self.conv2 = ARMAConv(hidden_channels, out_channels, num_stacks=3,
                               num_layers=2, shared_weights=True, dropout=0.25,
                               act=act_fn).jittable()
-
+        self.shape_x = None
+        self.index_size = None
     def forward(self, x, edge_index):
+
+        if(self.index_size == None):
+            self.index_size = edge_index.size()
+        else:
+            if(self.index_size != edge_index.size()):
+                print("dynamic shape")
+
+
+        if(self.shape_x == None):
+            self.shape_x = [x.size()]
+        else:
+            if(self.shape_x[0] != x.size()):
+                print("dynamic shape")
+
         x = F.dropout(x, training=self.training)
+        if(len(self.shape_x) == 1):
+            self.shape_x.append(x.size())
+        else:
+            if(self.shape_x[1] != x.size()):
+                print("dynamic shape")
+
         x = F.relu(self.conv1(x, edge_index))
+        if(len(self.shape_x) == 2):
+            self.shape_x.append(x.size())
+        else:
+            if(self.shape_x[2] != x.size()):
+                print("dynamic shape")
+
+
         x = F.dropout(x, training=self.training)
+        if(len(self.shape_x) == 3):
+            self.shape_x.append(x.size())
+        else:
+            if(self.shape_x[3] != x.size()):
+                print("dynamic shape")
+
         x = self.conv2(x, edge_index)
+        if(len(self.shape_x) == 4):
+            self.shape_x.append(x.size())
+        else:
+            if(self.shape_x[4] != x.size()):
+                print("dynamic shape")
+
         return F.log_softmax(x, dim=1)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model, data = Net(dataset.num_features, 16,
                   dataset.num_classes).to(device), data.to(device)
-model = torch.compile(model)
+# model = torch.jit.script(model)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
 
 def train():
     model.train()
     optimizer.zero_grad()
-    out = model(data.x, data.edge_index)
-    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-    loss.backward()
-    optimizer.step()
 
+    torch.cuda.synchronize()
+    start1 = time.time()
+
+    out = model(data.x, data.edge_index)
+    torch.cuda.synchronize()
+    stop1 = time.time()
+
+    loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+
+    torch.cuda.synchronize()
+    start2 = time.time()
+    loss.backward()
+    torch.cuda.synchronize()
+    stop2 = time.time()
+
+    torch.cuda.synchronize()
+    start3 = time.time()
+    optimizer.step()
+    torch.cuda.synchronize()
+    stop3 = time.time()
+
+    return stop1 - start1, stop2 - start2, stop3 - start3
 
 def test():
     model.eval()
+
+    torch.cuda.synchronize()
+    start = time.time()
+
     out, accs = model(data.x, data.edge_index), []
+    
+    torch.cuda.synchronize()
+    stop = time.time()
+
     for _, mask in data('train_mask', 'val_mask', 'test_mask'):
         pred = out[mask].argmax(1)
         acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
         accs.append(acc)
-    return accs
+    return accs, stop-start
 
 
 best_val_acc = test_acc = 0
 wramup = 10
 test_epoc = 100
-for epoch in range(1, wramup + test_epoc):
-    if(epoch == wramup):
+
+min_time = 100000000
+forward_min_time = 100000000
+train_forward_time = 100000000
+train_backward_time = 100000000
+train_upgrad_time = 100000000
+
+with torch.jit.fuser("fuser2"):
+    for epoch in range(1, wramup + test_epoc):
+        
+        torch.cuda.synchronize()
         start = time.time()
-    train()
-    train_acc, val_acc, tmp_test_acc = test()
-    if val_acc > best_val_acc:
-        best_val_acc = val_acc
-        test_acc = tmp_test_acc
-    print(f'Epoch: {epoch:03d}, Train: {train_acc:.4f}, '
-          f'Val: {best_val_acc:.4f}, Test: {test_acc:.4f}')
+
+        t1, t2, t3 = train()
+        
+        acc, forward_time = test()
+
+        torch.cuda.synchronize()
+        stop = time.time()
+
+        train_acc, val_acc, tmp_test_acc = acc
+
+        min_time = min(min_time, stop - start)
+        forward_min_time = min(forward_time, forward_min_time)
+        train_forward_time = min(t1, train_forward_time)
+        train_backward_time = min(t2, train_backward_time)
+        train_upgrad_time = min(t3, train_upgrad_time)
+        
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            test_acc = tmp_test_acc
+        print(f'Epoch: {epoch:03d}, Train: {train_acc:.4f}, '
+            f'Val: {best_val_acc:.4f}, Test: {test_acc:.4f}')
 ret = _cudart.cudaProfilerStop()
 
 torch.cuda.synchronize()
 stop = time.time()
 
-print("total =", stop-start)
-print("each =", (stop-start) / test_epoc)
+print("e2e_min = ", min_time)
+print("forward = ", forward_min_time)
+print("train_forward_time = ", train_forward_time)
+print("train_backward_time = ", train_backward_time)
+print("train_upgrad_time = ", train_upgrad_time)
+# print("each =", (stop-start) / test_epoc)
